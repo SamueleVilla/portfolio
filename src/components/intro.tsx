c75197a0ce1e9e46c947aea3b09153f16c51a67a
samuelevilla <samuele.villa.ufficiale@gmail.com>
2023-10-06 19:02:21 +0200

    cahanged cv source

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -90,8 +90,9 @@ export default function Intro() {
         </Link>
 
         <a
-          href="/cv-samuele-villa.pdf"
+          href="https://samuele-portfolio-images.s3.eu-central-1.amazonaws.com/cv/cv-samuele-villa.pdf"
           download
+          target="_blank"
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full
           outline-none focus:scale-110 hover:scale-110 active:scale-105 transition
           borderBlack"
